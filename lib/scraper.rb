d78e4f59806f7c43dce608ae2f5b8f2bcb4d0e3d
Nerly Ton <nerlyton@gmail.com>
2020-08-13 17:20:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,8 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
  
-doc.css("id-3c239880-48cf-5ce7-84d0-578546d87586.inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
+doc.css("#3c239880-48cf-5ce7-84d0-578546d87586.inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
